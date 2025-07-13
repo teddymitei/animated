7546f247df5a99cd38edd6f0ceba43a442d29400
teddymitei <teddymitei@gmail.com>
2025-07-13 18:03:11 +0300

    update

--- src/components/ProjectDetails.jsx
+++ src/components/ProjectDetails.jsx
@@ -40,7 +40,7 @@ const ProjectDetails = ({
               ))}
             </div>
             <a className="inline-flex items-center gap-1 font-medium cursor-pointer hover-animation">
-              View Project{href}
+              View Project{" "}
               <img src="assets/arrow-up.svg" className="size-4" href={href} />
             </a>
           </div>
