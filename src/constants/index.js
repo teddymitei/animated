30cd842146c5975831393b27317d82b09b1cca9f
teddymitei <teddymitei@gmail.com>
2025-07-13 16:07:34 +0300

    update

--- src/constants/index.js
+++ src/constants/index.js
@@ -243,38 +243,43 @@ export const mySocials = [
 
 export const experiences = [
   {
-    title: "Software Developer",
-    job: "Security & Defense Projects",
-    date: "2021-2023",
+    title: "IT&T Intern",
+    job: "Kenya Power and Lighting Company",
+    date: "2023 May-2023 August",
     contents: [
-      "Enhanced application security and developed new features, adhering to standards set by the Passive Defense Organization and National Cyberspace Center.",
-      "Designed and implemented intuitive map interfaces using MapsUI, enhancing user experience and enabling seamless interactive map integration.",
-      "Developed applications for industrial automation, leveraging C++ and the Fatek API for PLC communication.",
-      "Enhanced responsiveness and usability of applications using Windows Forms and WPF frameworks.",
-      "Executed XML to SVG conversions using X-DOM, ensuring dynamic and efficient data visualization.",
+      "Software installation ",
+      "Switch configuration ",
+      "IP phone configuration",
+      "VPN configuration ",
+      "User support ",
+      "Fiber splicing",
+      "Patch cable maintenance ",
+      "ICT LAN support ",
+      "Troubleshooting ",
+      "Ping testing ",
+      "Workflow and work ethics" ,
+      "Customer service",
+      "Hardware preventive and breakdown maintenance ",
+      "Software preventive and breakdown maintenance ",
+      "Printer preventive and breakdown maintenance",
     ],
   },
   {
     title: "Back-End Developer",
     job: "Car Manufacture",
-    date: "2023-2024",
+    date: "2024 June-Present",
     contents: [
-      "Engineered systems for large-scale data ingestion and analysis, ensuring efficient data processing and storage.",
-      "Developed back-end systems enabling vehicle-to-cloud communication for telemetry, diagnostics, and remote control:",
-      "✅ Implemented secure APIs, following ISO 26262 automotive safety standards.",
-      "✅ Ensured data privacy for customers and partners through industry-compliant protocols.",
-      "✅ Delivered remote features like over-the-air updates, real-time tracking, and remote start capabilities.",
+      "Diagnosing and troubleshooting issues in CPUs, laptops, and desktops.",
+      "Repairing and replacing faulty components such as hard drives, RAM, and motherboards.",
+      "Resolving both hardware and software problems effectively.",
+      "Performing replacements of various computer components, such as displays and keyboards",
+      "Ensuring all repairs are completed to a high standard and within the specified time frame.",
+      "Selling laptops, desktops, and other computer accessories.",
+      "Providing excellent customer service and support.",
+      "Maintaining accurate records of repairs and sales transactions.",
     ],
   },
-  {
-    title: "Freelance Developer",
-    job: "Self-Employed",
-    date: "2025-Present",
-    contents: [
-      "Created a personal portfolio using Three.js, React, Vite, and WebAPI to showcase technical expertise.",
-      "Continuously enhancing technical skills and expanding expertise in modern web development and back-end technologies.",
-    ],
-  },
+  
 ];
 export const reviews = [
   {
