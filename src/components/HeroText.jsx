ba9d1dff2e53a20c32f087b0bf397c0025c4940c
teddymitei <teddymitei@gmail.com>
2025-07-13 08:37:03 +0300

    update

--- src/components/HeroText.jsx
+++ src/components/HeroText.jsx
@@ -61,7 +61,7 @@ const HeroText = () => {
           animate="visible"
           transition={{ delay: 1 }}
         >
-          Hi,I'm Ali
+          Hi,I'm Teddy
         </motion.p>
         <div>
           <motion.p
