8e2f0c5b1ecd997e1c3fe1fc0f12392a70f1025a
teddymitei <teddymitei@gmail.com>
2025-07-13 17:36:07 +0300

    update

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -15,7 +15,8 @@ const Project = ({
     <>
       <div id="work"
         className="flex-wrap items-center justify-between py-10 space-y-14 sm:flex sm:space-y-0"
-
+        onMouseEnter={() => setPreview(image)}
+        onMouseLeave={() => setPreview(null)}
       >
         <div>
           <p className="text-2xl">{title}</p>
